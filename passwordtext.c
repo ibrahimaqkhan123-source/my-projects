5dcba23dfc90ff33a29722025614faf97db4fb9c
ibrahimaqkhan123-source <ibrahimaqkhan123@gmail.com>
2026-09-20 12:54:21 +0500

    Add password check in passwordtext.c

---
+++ passwordtext.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int password;
+
+  printf("What is your password?");
+  scanf("%d", password);
+  // This is the function you can edit if you want.
+  if (password == 12345) {
+    printf("that is the correct password.");
+  } else {
+  printf("Incorrect!");
+  }
+
+return 0;
+
+}
+    
+  
